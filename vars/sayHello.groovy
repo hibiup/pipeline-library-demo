ced795d5f6e602c412f3d4763e2fc1932a8e2342
Jeff W <jeffwjii@gmail.com>
2023-01-30 14:54:28 -0500

    import class in entry function

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,6 +1,15 @@
 #!/usr/bin/env groovy
 
-def call(String name = 'human') {
+import com.cleverbuilder.GlobalVars
+import com.cleverbuilder.SampleClass
+
+def call( String name = 'human') {
   echo "Hello, ${name}."
+
+  def sampleClass = new SampleClass()
+  sampleClass.setName(GlobalVars.foo)
+  sampleClass.setAge(23)
+
+  echo "Happy Birthday! ${sampleClass.getAge()}"
 }
 
